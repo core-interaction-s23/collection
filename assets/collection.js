aa6614ed357cd6d2a02b03cc67171c584612799a
Michael Fehrenbach <mf@michaelfehrenbach.com>
2023-03-07 23:28:43 -0500

    Add conditional example.

--- assets/collection.js
+++ assets/collection.js
@@ -33,7 +33,10 @@ const renderItems = (collection) => {
 			`
 		listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
 
-
+		// You can build logic from your data, too
+		if (!item.alsoWriter) { // If this is `false`
+			listItem.classList.add('faded') // Add this class to the whole `li`
+		}
 
 		collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
 	})
