d874ab23894a2adf3e4a5c937589ffb700486942
Michael Fehrenbach <mf@michaelfehrenbach.com>
2023-03-07 23:04:46 -0500

    Add template literal example.

--- assets/collection.js
+++ assets/collection.js
@@ -3,9 +3,13 @@ const renderItems = (collection) => {
 	// The `ul` where the items will be inserted
 	const collectionList = document.getElementById('collection')
 
+
+
 	// Loop through each item in the collection array
 	collection.forEach(item => {
 		const listItem = document.createElement('li') // Make the `li`
+
+
 
 		// You can make each element inside of that…
 		const itemTitle = document.createElement('h2') // Make an `h2`
@@ -15,6 +19,21 @@ const renderItems = (collection) => {
 		const itemImage = document.createElement('img') // And an image
 		itemImage.src = item.posterImage // Set the `src` attribute from the JSON
 		listItem.appendChild(itemImage) // And add that too
+
+
+
+		// This can get annoying, so we can use “template literals” instead
+		const itemDetails =
+			`
+				<p>Released in <time>${item.year}</time></p>
+				<p><em>${item.runTime}</em></p>
+				<a href="${item.imdbLink}">
+					<p>${item.imdbRating} / 10</p>
+				</a>
+			`
+		listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
+
+
 
 		collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
 	})
